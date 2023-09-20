fa155b62824e8739f14076ec9e2b890320864c6a
saeid <saeid.sepehrik@gmail.com>
2023-09-20 19:57:06 +0200

    change button name

--- FrontEnd/src/components/newGame/badgeRole.tsx
+++ FrontEnd/src/components/newGame/badgeRole.tsx
@@ -1,49 +1,49 @@
-import { Avatar, Badge, Button, Col, Row } from 'antd';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { incrementByAmount } from './step.slice';
-
-
+import { Avatar, Badge, Button, Col, Row } from "antd";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { incrementByAmount } from "./step.slice";
 
 export interface dataProps {
-    group: string[],
-    groupCount: number[],
+  group: string[];
+  groupCount: number[];
 }
-
 
 export const BadgeRole = ({ group, groupCount }: dataProps) => {
-    const step = useAppSelector(s=>s.step);
-    const dispatch = useAppDispatch();
-    return (
+  const step = useAppSelector((s) => s.step);
+  const dispatch = useAppDispatch();
+  return (
+    <>
+      {step.value === 2 && (
         <>
-            {
-                step.value === 2 &&
-                <>
-                    <hr />
-                    <Row>
-                        <Col span={12}>
+          <hr />
+          <Row>
+            <Col span={12}></Col>
+            <Col span={12}>
+              <Row>
+                {group.map((e: string) => (
+                  <Col span={7} key={Math.random()}>
+                    <Badge
+                      count={groupCount[group.indexOf(e)]}
+                      key={Math.random()}
+                      overflowCount={999}
+                    >
+                      <Avatar shape="square" size="large">
+                        {e}
+                      </Avatar>
+                    </Badge>
+                  </Col>
+                ))}
 
-                        </Col>
-                        <Col span={12}>
-                            <Row>
-                                {group.map((e: string) => (
-                                    <Col span={7} key={Math.random()}>
-                                        <Badge count={groupCount[group.indexOf(e)]} key={Math.random()} overflowCount={999}>
-                                            <Avatar shape="square" size="large">{e}</Avatar>
-                                        </Badge>
-                                    </Col>
-                                ))}
-
-                                <Col span={10}>
-                                    <Button onClick={() => dispatch(incrementByAmount(3))}>create</Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <hr />
-                </>
-
-            }
-
+                <Col span={10}>
+                  <Button onClick={() => dispatch(incrementByAmount(3))}>
+                    next
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <hr />
         </>
-    )
-}
+      )}
+    </>
+  );
+};
