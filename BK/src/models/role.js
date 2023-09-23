11903d5d33ac86f7b29b7c27a4c65d3de1ed1ccd
saeid <saeid.sepehrik@gmail.com>
2023-09-23 22:24:12 +0200

    fix role server model

--- BK/src/models/role.js
+++ BK/src/models/role.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    title_fn: { type: String, required: true },
-    is_active: { type: Boolean, default: true },
-    mask_code_scenarios: { type: Number, required: true },
-    color: { type: String, required: true },
-    group: { type: String, required: true },
-    just_one: { type: Boolean, required: true },
-    pic_path: String,
-    dsc: String,
+  _id: { type: String, required: true },
+  title: { type: String, required: true },
+  title_fn: { type: String, required: true },
+  is_active: { type: Boolean, default: true },
+  mask_code_scenarios: { type: Number, required: true },
+  color: { type: String, required: true },
+  group: { type: String, required: true },
+  just_one: { type: Boolean, required: true },
+  pic_path: String,
+  dsc: String,
 });
 
-const role = mongoose.model('role', roleSchema);
+const role = mongoose.model("role", roleSchema);
 
 module.exports = role;
