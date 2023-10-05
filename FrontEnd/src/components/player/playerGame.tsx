9b4fbbd7539beaf444b5654870c21bbd7a83c688
saeid <saeid.sepehrik@gmail.com>
2023-10-05 05:57:19 +0200

    fixed bug

--- FrontEnd/src/components/player/playerGame.tsx
+++ FrontEnd/src/components/player/playerGame.tsx
@@ -1,14 +1,16 @@
-// import React from "react";
-// import { useEffect } from "react";
+import React from "react";
 import { WaitingPlayer } from "./waitingPlayer";
 import { HoldGamePlayer } from "./holdGamePlayer";
 import { useAppSelector } from "../../redux/hooks";
+
 export const PlayerGame = () => {
   const playerSelector = useAppSelector((s) => s.player);
   return (
     <>
-      {playerSelector.dataRoleGame._id === "" && <WaitingPlayer />}
-      {playerSelector.dataRoleGame._id !== "" && <HoldGamePlayer />}
+      {localStorage.getItem("idRoleGamePlayer") === null &&
+        playerSelector.dataRoleGame._id === "" && <WaitingPlayer />}
+      {(localStorage.getItem("idRoleGamePlayer") !== null ||
+        playerSelector.dataRoleGame._id !== "") && <HoldGamePlayer />}
     </>
   );
 };
