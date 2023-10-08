137b2ef49f351d227ef36ef616f2adbf1b95b118
saeid <saeid.sepehrik@gmail.com>
2023-10-09 00:06:28 +0200

    change leyout

--- FrontEnd/src/components/pages/AppLayout.tsx
+++ FrontEnd/src/components/pages/AppLayout.tsx
@@ -1,43 +1,92 @@
-import { Layout } from "antd";
-import { Route, Routes } from "react-router-dom";
+import { UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
+
+import { Layout, Menu, theme } from "antd";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Home } from "./Home";
 import { NewGame } from "../newGame/newGame";
-import { Footer } from "antd/es/layout/layout";
 import { Login } from "../auth/Login";
-import { Main } from "../dashbord/main";
-import { ProtectedRout } from "./protectedRout";
-import { HeaderComponent } from "./headerComponent";
 import { Game } from "../game/game";
 import { JoinGame } from "../player/joinGame";
 import { PlayerGame } from "../player/playerGame";
-
-const { Header, Content } = Layout;
+import { ProtectedRout } from "./protectedRout";
+import { Main } from "../dashbord/main";
+import { useState } from "react";
+const { Header, Content, Footer, Sider } = Layout;
 
 export const AppLayout = () => {
+  const navigate = useNavigate();
+  const [collapsedState, setcollapsedState] = useState(true);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
     <Layout>
-      <Header>
-        <HeaderComponent />
-      </Header>
-      <Content>
-        <Routes>
-          <Route path="/" element={<Home></Home>} />
-          <Route path="/newGame" element={<NewGame />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/joinGame" element={<JoinGame />} />
-          <Route path="/playerGame" element={<PlayerGame />} />
-          <Route
-            path="/dashbord"
-            element={
-              <ProtectedRout>
-                <Main />
-              </ProtectedRout>
-            }
-          />
-        </Routes>
-      </Content>
-      <Footer>Footer</Footer>
+      <Sider
+        collapsed={collapsedState}
+        breakpoint="lg"
+        collapsedWidth="0"
+        onCollapse={() => {
+          setcollapsedState(!collapsedState);
+        }}
+      >
+        {/* <div className="demo-logo-vertical" /> */}
+        <Menu
+          onClick={(info) => {
+            setcollapsedState(!collapsedState);
+            navigate(info.key);
+          }}
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["/"]}
+          items={[
+            {
+              key: "/",
+              icon: <UserOutlined />,
+              label: "Home",
+            },
+            {
+              key: "/newGame",
+              icon: <VideoCameraOutlined />,
+              label: "New Game",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          header
+        </Header>
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<Home></Home>} />
+              <Route path="/newGame" element={<NewGame />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/game" element={<Game />} />
+              <Route path="/joinGame" element={<JoinGame />} />
+              <Route path="/playerGame" element={<PlayerGame />} />
+              <Route
+                path="/dashbord"
+                element={
+                  <ProtectedRout>
+                    <Main />
+                  </ProtectedRout>
+                }
+              />
+            </Routes>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
     </Layout>
   );
 };
